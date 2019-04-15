54a5d6f3c3ea3d56e4df8e28332b0b001d620d53
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2019-04-15 17:50:11 +0200

    Allow Run to be extended

--- src/main/rummikub/Run.kt
+++ src/main/rummikub/Run.kt
@@ -1,6 +1,26 @@
 package main.rummikub
 
 data class Run(val tiles: List<Tile>) {
+
+    init {
+        val moreThanOneSuit = tiles.distinctBy { it.suit }.size > 1
+        if (moreThanOneSuit) throw NotUniqueSuitInRunException(tiles)
+    }
+
+    fun extendWith(candidate: Tile): Run {
+        val fitsOnTheLeft = candidate.value == minValue() - 1
+        val fitsOnTheRight = candidate.value == maxValue() + 1
+
+        return when {
+            fitsOnTheLeft -> Run(listOf(candidate).plus(tiles))
+            fitsOnTheRight -> Run(tiles.plus(candidate))
+            else -> this
+        }
+    }
+
+    private fun minValue() = tiles.minBy { it.value }!!.value
+    private fun maxValue() = tiles.maxBy { it.value }!!.value
+
     override fun toString(): String {
         val values = tiles.joinToString(
             separator = "-",
@@ -23,3 +43,10 @@ fun IntRange.red() = this.map { value -> value.red() }
 fun IntRange.blue() = this.map { value -> value.blue() }
 fun IntRange.green() = this.map { value -> value.green() }
 fun IntRange.yellow() = this.map { value -> value.yellow() }
+
+class NotUniqueSuitInRunException(tiles: List<Tile>) : Throwable(
+    """
+    A run can only contain one suit.
+    Received $tiles
+    """.trimIndent()
+)
--- src/test/rummikub/RunTest.kt
+++ src/test/rummikub/RunTest.kt
@@ -1,8 +1,11 @@
 package test.rummikub
 
 import main.rummikub.*
+import org.hamcrest.CoreMatchers.containsString
+import org.junit.Assert.assertThat
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class RunTest {
 
@@ -12,6 +15,36 @@ class RunTest {
         assertEquals("[1-2-3][blue]", Run((1..3).blue()).toString())
         assertEquals("[8-9-10-11-12][green]", Run((8..12).green()).toString())
         assertEquals("[7-8-9][yellow]", Run((7..9).yellow()).toString())
+    }
+
+    @Test
+    fun a_run_with_different_suits_cant_be_created() {
+        var thrown = false
+        try {
+            Run(listOf(1.red(), 2.green()))
+        } catch (e: NotUniqueSuitInRunException) {
+            thrown = true
+            assertThat(e.message, containsString("red"))
+            assertThat(e.message, containsString("green"))
+        } finally {
+            assertTrue(thrown)
+        }
+    }
+
+    @Test
+    fun a_run_can_be_extended_by_the_left_with_same_suit() {
+        assertEquals(
+            Run((3..6).red()),
+            Run((4..6).red()).extendWith(3.red())
+        )
+    }
+
+    @Test
+    fun a_run_can_be_extended_by_the_right_with_same_suit() {
+        assertEquals(
+            Run((8..12).blue()),
+            Run((8..11).blue()).extendWith(12.blue())
+        )
     }
 }
 
